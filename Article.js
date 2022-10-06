66fc01d461a82c794e7a260cf2bc7ed331150231
elzp <86740659+elzp@users.noreply.github.com>
2022-10-06 18:34:16 +0200

    Get id of article in parent component

--- Article.js
+++ Article.js
@@ -12,7 +12,8 @@ export default {
           <span class="text-grey-darkest font-thin text-xl">
            {{index}}Massa vitae tortor condimentum lacinia quis vel eros donec
           </span>
-          <div class="rounded-full border border-grey w-7 h-7 flex items-center justify-center">
+          <div class="rounded-full border border-grey w-7 h-7 flex items-center justify-center"
+          @click="()=>$emit('change',this.index)">
               <!-- icon by feathericons.com -->
               <svg aria-hidden="true" class="" data-reactid="266" fill="none" height="24" stroke="#606F7B" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewbox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
                   <polyline points="6 9 12 15 18 9">
--- index.js
+++ index.js
@@ -2,9 +2,14 @@ import { createApp } from 'vue';
 import Article from './Article.js';
 const app = createApp({
   components: { Article },
+  methods: {
+    changeVisibility(id) {
+      console.log(id);
+    },
+  },
   template: `<main class="w-3/5 p-8 mx-auto">
             <section class="shadow" v-for="id in [1,2,3,4]">
-                <Article :index="id"/>
+                <Article :index="id" @change="changeVisibility" />
             </section>
         </main>`,
 });
